9313f8cead561569348d2474fcd7e31bc7833399
Afifa Khan11 <faujiaafifakhan@gmail.com>
2023-08-21 16:50:56 +0600

    first commit

---
+++ js/login.js
@@ -0,0 +1,12 @@
+console.log('login js file');
+document.getElementById(btn-submit) .addEventListener ('click' ,function (){
+// console.log('submit button clicked');
+})
+
+function handleClikBtn (target){
+//     console .log(target.parentNode.childNode);
+// const itemName=target.parentNode;
+const li =document.createElement('li');
+}
+    
+
